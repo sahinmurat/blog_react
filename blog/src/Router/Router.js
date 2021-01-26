6ec49664922feebbaf2cdb6216119f3ed1ef5332
Murat Sahin <yazilimogren1111@gmail.com>
2021-01-26 19:38:19 +0100

    router created

---
+++ blog/src/Router/Router.js
@@ -0,0 +1,25 @@
+import {BrowserRouter as Router , Switch, Route} from "react-router-dom";
+import React from 'react';
+import Signup from '../pages/Signup'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import Signin from "../pages/Signin";
+import Profile from "../pages/Profile";
+import Main from "../pages/Main";
+
+function AppRouter() {
+    return (
+        <Router>
+            <Navbar />
+            <Switch>
+                <Route exact path='/signup' component={Signup} />
+                <Route exact path='/signin' component={Signin} />
+                <Route exact path='/profile' component={Profile} />
+                <Route exact path='/' component={Main} />
+            </Switch>
+            <Footer />
+        </Router>
+    )
+}
+
+export default AppRouter
---
+++ blog/src/pages/Signin.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Signin() {
+    return (
+        <div>
+            Signin
+        </div>
+    )
+}
+
+export default Signin
