9bbbf1e151013ccd2255378cd9b2a02392e4de1b
Murat Sahin <yazilimogren1111@gmail.com>
2021-01-26 22:16:25 +0100

    api tested

--- blog/src/pages/Main.js
+++ blog/src/pages/Main.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
+import axios from 'axios'
 
 function Main() {
+    const [api, setApi] = useState('')
+    useEffect( async () => {
+        try {
+            const response = await axios.get('https://sahinblog.herokuapp.com/list');
+            console.log(response.data.results)
+            setApi(response.data.results)
+        } catch (err) {
+            console.error(err)
+        }
+    }, [])
+
+
+
     return (
         <div>
-            Main
+            {api.map((neu) =>(
+                <p>{neu.author} </p>
+            ))}
+            {/* {JSON.stringify(api)} */}
         </div>
     )
 }
