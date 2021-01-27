0e144d4ec5fb0bc959df7124686cb9d6e00ed02d
Murat Sahin <yazilimogren1111@gmail.com>
2021-01-27 21:40:23 +0100

    material ui added

--- blog/src/pages/Signup.js
+++ blog/src/pages/Signup.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import {Button, TextField, Grid, Container} from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
+
+const styles = makeStyles({
+    wrapper:{
+        marginTop: '5rem'
+    
+    }
+})
 
 function Signup() {
+    const signupStyles = styles();
     return (
         <div>
-        Signup
+            <Container className={signupStyles.wrapper} maxWidth="sm">
+            <Grid container spacing={3}> 
+                <Grid item xs={12} >
+                    <TextField id="outlined-basic" label="Username" variant="outlined" fullWidth  />
+                </Grid>
+                <Grid item xs={12} >
+                    <TextField id="outlined-basic" label="Password" variant="outlined" fullWidth  />
+                </Grid>
+                <Grid item xs={12} >
+                    <Button variant="contained" color="primary" fullWidth > SignUp </Button>
+                </Grid>
+                <Grid item xs={12} >
+                    <Button variant="contained" color="primary" fullWidth > Primary </Button>
+                </Grid>
+            </Grid>
+            </Container>
         </div>
     )
 }
