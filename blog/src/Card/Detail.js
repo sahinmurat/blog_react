b28e70c390c0db8c3b8e024da0144df5ce845dab
Murat Sahin <yazilimogren1111@gmail.com>
2021-01-30 23:15:08 +0100

    detail page updated

--- blog/src/Card/Detail.js
+++ blog/src/Card/Detail.js
@@ -1,38 +1,32 @@
-import React from 'react'
+import React , {useState, useEffect} from 'react'
+import axios from 'axios'
 import {useParams} from 'react-router-dom'
 
 function Detail() {
+    const [detail, setDetail] = useState('')
     const {slug} = useParams();
     console.log('slug', slug)
     const token = 'cac3a18ef501580fa9e7aa0d82457e586b167a26'
 
-    // const handleSubmit = () => {
-    //   axios.post('https://sahinblog.herokuapp.com/create', {
-    //     title:title , content:content, image:image, category:category
-    //   }, {
-    //            headers:{
-    //              'Authorization':`Token ${token}`
-    //            }
-    //          }
-    //   ).then((a)=> console.log(a))
-    //   .catch((a)=> console.log(a))
-    // }
     useEffect( async () => {
        
-        axios.post('https://sahinblog.herokuapp.com/create', {
-            title:title , content:content, image:image, category:category
-          }, {
+        axios.get(`https://sahinblog.herokuapp.com/${slug}/detail-comment`,  {
                    headers:{
                      'Authorization':`Token ${token}`
                    }
                  }
-          ).then((a)=> console.log(a))
+          ).then((a)=> setDetail(a.data))
           .catch((a)=> console.log(a))
 }, [])
-
+    console.log('detail', detail)
     return (
         <div>
-            <p> {slug} </p>
+            <p><b> {detail.title} </b></p>
+            <p> {detail.content} </p>
+            <img src = {detail.image} alt= 'photo' />
+            <p> {detail.author} </p>
+         
+            
         </div>
     )
 }
