855a2b4e40775f67418063f204d2fa117bee4f99
Murat Sahin <yazilimogren1111@gmail.com>
2021-01-30 23:57:03 +0100

    update

--- blog/src/Categories/Politic.js
+++ blog/src/Categories/Politic.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
+import  Card  from '../Card/Card'
 
-function Politic() {
+function Main() {
+    const [res, setRes] = useState('')
+    const [culture, set] = useState('')
+    const politic = []
+    console.log(res)
+    useEffect( async () => {
+       
+            const response = await axios.get('https://sahinblog.herokuapp.com/list')
+            .then(
+               (res)=>  setRes(res.data.results)
+            )
+            .catch(
+                (err)=> console.error(err)
+            )
+    }, [])
     return (
         <div>
-            Politic
+             {res ? res.map((neu) =>(neu.category == 'Politic' 
+             ? politic.push(neu) : null
+             )) : <p>Loading...</p>}
+             { politic.map((a) =>  < Card item = {a} /> )}
         </div>
     )
 }
 
-export default Politic
+export default Main
